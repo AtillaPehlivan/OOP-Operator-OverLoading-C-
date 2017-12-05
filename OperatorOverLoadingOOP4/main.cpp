6f7ed299f3b87ed03551a1ccd5335df34c0fb9b3
Atila Pehlivan <web.atillapehlivan@gmail.com>
2017-12-05 19:17:16 +0300

    Initial Commit

---
+++ OperatorOverLoadingOOP4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OperatorOverLoadingOOP4
+//
+//  Created by Remzi Hoca on 5.12.2017.
+//  Copyright © 2017 Atilla Pehlivan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
